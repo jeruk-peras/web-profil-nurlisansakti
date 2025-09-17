b9476063a1138460d7ba32a4ac64796911fa7c4d
Jerik Peras <prakosoadimas032@gmail.com>
2025-09-17 14:07:14 +0700

    md: footer FE

--- app/Views/layout/footer.php
+++ app/Views/layout/footer.php
@@ -1,88 +1,4 @@
- <footer class="footer-bg footer-p pt-90" style="background-color: #0c2957; background-image: url(/img/bg/footer-bg.png);">
-     <div class="footer-top pb-70">
-         <div class="container">
-             <div class="row justify-content-between">
-
-                 <div class="col-xl-4 col-lg-4 col-sm-6">
-                     <div class="footer-widget mb-30">
-                         <div class="f-widget-title">
-                             <h2>About Company</h2>
-                         </div>
-                         <div class="f-contact">
-                             <p>Suspendisse non sem ante. Cras pretium gravida leo a convallis. Nam malesuada interdum metus, sit amet dictum ante congue eu. Maecenas ut maximus enim.</p>
-
-                         </div>
-                         <div class="footer-social mt-10">
-                             <a href="#"><i class="fab fa-facebook-f"></i></a>
-                             <a href="#"><i class="fab fa-instagram"></i></a>
-                             <a href="#"><i class="fab fa-twitter"></i></a>
-                         </div>
-                     </div>
-                 </div>
-                 <div class="col-xl-2 col-lg-2 col-sm-6">
-                     <div class="footer-widget mb-30">
-                         <div class="f-widget-title">
-                             <h2>Our Links</h2>
-                         </div>
-                         <div class="footer-link">
-                             <ul>
-                                 <li><a href="index.html">Home</a></li>
-                                 <li><a href="about.html"> About</a></li>
-                                 <li><a href="services.html"> Services </a></li>
-                                 <li><a href="contact.html"> Contact Us</a></li>
-                                 <li><a href="blog.html">Blog </a></li>
-                             </ul>
-                         </div>
-                     </div>
-                 </div>
-                 <div class="col-xl-3 col-lg-3 col-sm-6">
-                     <div class="footer-widget mb-30">
-                         <div class="f-widget-title">
-                             <h2>Latest Post</h2>
-                         </div>
-                         <div class="recent-blog-footer">
-                             <ul>
-                                 <li>
-                                     <div class="thum"> <img src="/img/blog/s-blogimg-01.png" alt="img"></div>
-                                     <div class="text"> <a href="#">Nothing impossble to need hard work</a>
-                                         <span>7 March, 2020</span>
-                                     </div>
-                                 </li>
-
-                                 <li>
-                                     <div class="thum"> <img src="/img/blog/s-blogimg-02.png" alt="img"></div>
-                                     <div class="text"> <a href="#">Nothing impossble to need hard work</a>
-                                         <span>7 March, 2020</span>
-                                     </div>
-                                 </li>
-
-                             </ul>
-
-
-                         </div>
-                     </div>
-                 </div>
-                 <div class="col-xl-3 col-lg-3 col-sm-6">
-                     <div class="footer-widget mb-30">
-                         <div class="f-widget-title">
-                             <h2>Subscribe Now !</h2>
-                         </div>
-                         <div class="footer-link">
-                             <p>Subscribe our newsletter to get our latest update & News</p>
-                             <div class="subricbe p-relative" data-animation="fadeInDown" data-delay=".4s">
-                                 <form action="https://htmldemo.zcubethemes.com/autozox/news-mail.php" method="post" class="contact-form ">
-                                     <input type="text" id="email2" name="email2" class="header-input" placeholder="Your Email..." required>
-                                     <button class="btn header-btn"><i class="fas fa-location-arrow"></i></button>
-                                 </form>
-                             </div>
-                         </div>
-
-                     </div>
-                 </div>
-
-             </div>
-         </div>
-     </div>
+ <footer class="footer-bg footer-p">
      <div class="copyright-wrap">
          <div class="container">
              <div class="row align-items-center">
@@ -91,17 +7,17 @@
                      <img src="/img/logo/f_logo.png" alt="img">
                  </div>
                  <div class="col-lg-4">
-                     <div class="copy-text">
-                         Copyright &copy; Autozox 2022 . All rights reserved.
+                     <div class="copy-text text-center align-middle">
+                         Copyright &copy; NUR LISAN SAKTI 2025.
                      </div>
                  </div>
 
                  <div class="col-lg-4 text-right text-xl-right">
-                     <ul>
-                         <li><a href="#">Privacy</a></li>
-                         <li><a href="#">Term &amp; Conditions</a></li>
-                         <li><a href="#">Legal</a></li>
-                     </ul>
+                     <div class="footer-social mt-10">
+                         <a href="#"><i class="fab fa-facebook-f"></i></a>
+                         <a href="#"><i class="fab fa-instagram"></i></a>
+                         <a href="#"><i class="fab fa-twitter"></i></a>
+                     </div>
                  </div>
              </div>
          </div>
